31c8b07470da8628101b0744ff61eef0a93806bf
batron68 <56423847+batron68@users.noreply.github.com>
2019-10-11 23:39:23 -0700

    Add files via upload

---
+++ src/budget.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+int main()
+
+    {
+   string username, password, address;
+    cout << "Enter your Name: ";
+    getline(cin,username);
+    cout << "Password: ";
+    getline(cin,password);
+    cout << "Enter your Address: ";
+    getline(cin,address);
+    
+    cout << "Hi I am " << username <<", I live at " << address <<"\n";
+
+    return 0;
+    }
---
+++ src/section.cpp
@@ -0,0 +1,15 @@
+
+#include <iostream>
+
+using namespace std;
+int main()
+
+    {
+    string Ursection;
+    cout << "Enter your Section : ";
+    cin >>  Ursection;
+    cout << "*** Section: " << Ursection << " ***\n";
+
+
+    return 0;
+    }
